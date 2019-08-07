787d01cfd10fb78acec67275636e17be2357671d
shanaslavin <shanaslavin@gmail.com>
2019-08-07 11:52:33 -0400

    MVP working

--- week2/day3/nasa/nasa/src/components/Apod.js
+++ week2/day3/nasa/nasa/src/components/Apod.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const API_KEY = "7ywfyjWGHRMiqDqmjTcxsrJMZkaeo0Z4RgftoNxw";
 
 const Apod = ({ match }) => {
 
   const date = match.params.date;
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setError(false);
+    setLoading(true);
+    axios.get(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${API_KEY}`)
+    .then((response) => {
+    setData(response.data);
+    setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+      setError(true);
+    })
+  }, [date])
+
+  if(loading) return(<div>Loading</div>);
+  if(error) return(<div>Error!</div>)
 
   return(
     <div>
-      <h1>Hello World!</h1>
+      <h2>{data.title}</h2>
+      <em>{data.copyright}</em>
+      <br></br>
+      <img src={data.url} alt={data.title}/>
+      <p>{data.explanation}</p>
     </div>
   )
 }
